bcbf640850a00f2d7f82e9a35e7f0a3d991f02d4
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-06-02 13:39:13 +0100

    Added search bar and other fixes. Can't put magnifying glass on search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ chatroom = Chatroom.create!
 10.times do
   user = [guy, jose, raimundo, ricardo].sample
   boardgame = Boardgame.all.sample
-  Meeting.create!(user: user, boardgame: boardgame, start_date: 20220610, address:"Rua do Conde Redondo",
+  Meeting.create!(user: user, boardgame: boardgame, start_date: 20220610, start_time: Time.new(2022, 6, 10, 16, 00, 0), address:"Lisboa, Rua do Conde Redondo",
                   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                   name: "#{boardgame.name} night", chatroom: chatroom, players_wanted: 3)
 end
