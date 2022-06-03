89129da107e6073a14fc1caa581bf6d6491005f8
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-06-03 15:13:25 +0100

    Erased require byebug

--- app/validators/players_wanted_validator.rb
+++ app/validators/players_wanted_validator.rb
@@ -1,11 +1,8 @@
-require "byebug"
-
 class PlayersWantedValidator < ActiveModel::Validator
   def validate(record)
     max_players = record.meeting.players_wanted
     meeting = record.meeting
     number_of_players = meeting.users.count
-    # byebug
     if (number_of_players + 1) > max_players
       record.errors.add(
         :user_meeting,
