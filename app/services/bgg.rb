5486d025fdf5318d92a0cd15c08a0f1ab24971ac
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-06-07 15:33:36 +0100

    Working on it

--- app/services/bgg.rb
+++ app/services/bgg.rb
@@ -6,6 +6,7 @@ require "nokogiri"
 class Bgg
   def initialize
     @filepath = "app/services/archive/bgg_small_development.csv"
+    @person_filepth = "app/services/archive/bgg_Person.csv"
   end
 
   def call
@@ -13,11 +14,24 @@ class Bgg
       boardgame = Boardgame.create(bgg_id: row[:bgg_id], name: row[:name],
                       min_players: row[:min_players].to_i, max_players: row[:max_players].to_i,
                       rating: row[:avg_rating].to_f, complexity: row[:complexity].to_f, min_time: row[:min_time].to_i,
-                      max_time: row[:max_time].to_i
+                      max_time: row[:max_time].to_i,
+                      designers: row[:designer], artists: row[:artist]
                       )
     get_picture_and_description(boardgame)
+    update_designers_and_artists(boardgame)
     end
-    p "finished getting info from BBG"
+    p "finished getting info from BGG"
+  end
+
+  def update_designers_and_artists(boardgame)
+    CSV.foreach(@person_filepath, headers: :first_row, header_converters: :symbol) do |row|
+      if row[:bgg_id] == boardgame.designers
+        boardgame.update(designers: row[:name])
+      end
+      if row[:bgg_id].to_i == boardgame.artists
+        boardgame.update(artists: row[:name])
+      end
+    end
   end
 
   def get_picture_and_description(boardgame)
@@ -29,9 +43,10 @@ class Bgg
     # html_doc = Nokogiri::HTML(html_file)
     # max_players = document.search("maxplayers").attribute("value").text
     selected_boardgame = Boardgame.find(boardgame.id)
-    bgg_description = document.search("description").text
-    selected_boardgame.update(description: bgg_description)
     bgg_image = URI.open(document.search("image").text)
     selected_boardgame.photo.attach(io: bgg_image, filename: "#{boardgame.name}.png", content_type: 'image/png')
+    selected_boardgame = Boardgame.find(boardgame.id)
+    bgg_description = document.search("description").text
+    selected_boardgame.update(description: bgg_description)
   end
 end
