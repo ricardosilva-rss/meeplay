406e11a9a001d62fb9a8e4fc77f39b4f7579e494
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-06-09 20:05:14 +0100

    A2HS supposedly working

---
+++ app/javascript/components/ripple.js
@@ -0,0 +1,26 @@
+export const setupRipple = () => {
+  const btn = document.querySelectorAll('.material');
+
+  btn.forEach(el => {
+    el.style.position = 'relative';
+    el.style.overflow = 'hidden';
+    el.addEventListener('click', function(e) {
+      var x = e.offsetX;
+      var y = e.offsetY;
+
+      var ripples = document.getElementsByClassName('ripple');
+
+      if (ripples.length < 10) { // this restricts the user from creating lots of ripples
+        var ripple = document.createElement('span');
+        ripple.classList.add('ripple');
+        ripple.style.left = x+'px';
+        ripple.style.top = y+'px';
+        this.appendChild(ripple);
+
+        setTimeout(function() {
+          ripple.remove();
+        }, 1000);
+      }
+    });
+  });
+}
