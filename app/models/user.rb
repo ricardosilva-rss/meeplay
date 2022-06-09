5b0cdc52e784980eb380fd68d77332d8125fbbcf
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-06-09 01:25:41 +0100

    changed to seeds and sign up now creates a profile with default values

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :owned_meetings, foreign_key: "user_id", class_name: "Meeting"
@@ -10,4 +12,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :create_profile_for_user
+
+  def create_profile_for_user
+    profile = Profile.create(user: self, name: "Me", date_of_birth: Date.new(2000, 1, 1))
+    file4 = URI.open('https://avatars.githubusercontent.com/u/97455167?v=4')
+    profile.photo.attach(io: file4, filename: 'ricardo.jpg', content_type: 'image/jpg')
+  end
 end
